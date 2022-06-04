ff64051246839e681031ef7c25e4381353f27330
Artem Ignatov <ghettoarabdog@gmail.com>
2022-06-04 23:58:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HW_M_3._10._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число.");
+            int number = int.Parse(Console.ReadLine());
+            int i = 2;
+            while (i < number / 2 + 1)
+            {
+                if (number % i == 0)
+                {
+                    Console.Write("Число не является простым.");
+                    Console.ReadKey();
+                    return;
+                }
+                i++;
+            }
+            Console.Write("Число является простым.");
+            Console.ReadKey();
+        }
+    }
+}
